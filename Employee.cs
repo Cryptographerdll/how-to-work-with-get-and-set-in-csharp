d67dd1a7b972a143638d2399f15753a9cd728f44
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-09-28 18:12:36 +0100

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation_2
+{
+    class Employee
+    {
+        // Avant Encapsulation :
+
+        private int id;
+        private string nom;
+        private double salary;
+
+        // L'Encapsulation : 
+
+        public int ID
+        {
+            get { return id; }
+            set { this.id = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { this.nom = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set {
+                  if (value < 5000)
+                    value = 5000;
+                  
+                     this.salary = value;
+                }
+        }
+
+        private string MyPrintData()
+        {
+            return id + ";" + nom + ";" + salary;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine(MyPrintData().Replace(";", "-"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Encapsulation_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.ID = 111 ;
+            emp1.Nom = "khalil";
+            emp1.Salary = 2000;
+
+            emp1.PrintData();
+        }
+    }
+}
